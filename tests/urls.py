be68ef4f7fa75018dc67d8c437d2e45e9bbeaf74
Noumbissi valere Gille Geovan <noumbissivalere@gmail.com>
2019-07-12 20:25:42 +0100

    [API] Enabled APIs #5

--- tests/urls.py
+++ tests/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^library/', include('template_library.urls')),
-    url(r'^user/', include('openwisp_users.api.urls'))
+    url(r'^users/', include('openwisp_users.api.urls')),
+    url(r'^template/', include('openwisp_controller.config.api.urls'))
 ]
